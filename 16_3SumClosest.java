791df1bb8108eb68991370bc9622497c6fc72fc6
csis2020 <73615176+csis2020@users.noreply.github.com>
2022-09-22 02:34:18 -0700

    16_3SumClosest.java update

--- 16_3SumClosest.java
+++ 16_3SumClosest.java
@@ -1,13 +1,61 @@
-//2022.09.07
+
 // limitation: if nums is null or size is zero, which value should be returned??
 //Input: nums = [-1,2,1,-4], target = 1   -> Output: 2
 // Input: nums = [0,0,0], target = 1  -> Output: 0
 
 // Time Complexity: O(n^2) ( sorting 에 걸리는 시간은 O(n log n) , O(n^2)가 더 큼.)
 // Space Complexity: from O(logN) to O(n), depending on the implementation of the sorting algorithm.
+//2022-09-21
 
+class Solution{
+    public int threeSumClosest(int[] nums, int target){
+        if(nums == null || nums.length == 0){
+            return -1001; //?? 
+        }
+        
+        int diff = Integer.MAX_VALUE;
+        
+        //Sort to find 2 sum using min/max values
+        Arrays.sort(nums);
+        
+        int length = nums.length;
+        for(int i = 0; i < length-1; i++ ){
+            
+            int min = i + 1;
+            int max = length - 1;
+            
+            while(min < max){
+                
+                int sum = nums[i] + nums[min] + nums[max];
+                if(sum == target){
+                    return sum;
+                }
+                //Compare its absolute difference to previous absolute difference to find closest  3 sum
+                if(Math.abs(target - sum ) < Math.abs(diff)){
+                    diff = target - sum;
+                }
+            
+                if(sum < target){
+                    min++;
+                }else{
+                    max--;
+                }
+            }
+        }
+    
+        return (target - diff);
+    }
+}
+
+
+
+//2022.09.22 에 다시 아래 솔루션을 보니 어떻게 test 를 Pass 했는지 싶게..
+// 좀 이상하네. leetcode 솔루션 처럼 ClosestNum 을 while 문 안에서 check  하는게 맞는듯. 
+/*
+//2022.09.07
 //- two point 
-// leetcode 솔루션에서는 while 문에서 closest 를 비교했는데 그럴필요없이, for 문에서만 비교해도 됨. 이것이 더 낫다고 봄 
+// Time Complexity: O(n^2) ( sorting 에 걸리는 시간은 O(n log n) , O(n^2)가 더 큼.)
+// Space Complexity: from O(logN) to O(n), depending on the implementation of the sorting algorithm.
 
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
@@ -45,7 +93,7 @@ class Solution {
         return closestNum;
     }
 }
-
+*/
 // leetcode 솔루션중 binary search 를 이용한 경우  -- 이해하지는 못했음. 
 // Time Complexity: O(n^2 log n )
 // Space Complexity: from O(logN) to O(n), depending on the implementation of the sorting algorithm.
