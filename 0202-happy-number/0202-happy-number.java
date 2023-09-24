ef5b8c005613804c713cebb8f442ccec784192d2
Hye Kyung Ko <73615176+csis2020@users.noreply.github.com>
2023-09-23 19:54:58 -0700

    Time: 1 ms (84.81%), Space: 39.7 MB (36.26%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -13,31 +13,31 @@
 //  (만약 9999 라면 =>대충 10000 => log10000=4 (나누는 수가 10인 log) => log4 (< log10) = 대충 1,
 //  (즉, logN + log(logN) + log(log(logN))) ... 이렇게 됨. 
 // Space Complexity: O(1)
+/*
 class Solution {
     public boolean isHappy(int n) {
         if(n <= 0){
             return false;
         }
         
-        /*
-        int fast = n;
-        int slow = n;
-        while(fast != 1){
-            fast = getSum(getSum(fast));
-            slow = getSum(slow);
-            if(fast == slow){ //이렇게 하면 fast 와 slow 가 둘이 1로 같은경우 true 가 아닌 false 를 리턴하게 됨. 
-                return false;
-            }
-        }
-        */
+       
+        //int fast = n;
+        //int slow = n;
+        //while(fast != 1){
+        //    fast = getSum(getSum(fast));
+        //    slow = getSum(slow);
+        //    if(fast == slow){ //이렇게 하면 fast 와 slow 가 둘이 1로 같은경우 true 가 아닌 false 를 리턴하게 됨. 
+        //        return false;
+        //    }
+       // }
+      
         int fast = getSum(n); //to make that fast is different from slow.
         int slow = n;
         while(fast != 1 && fast != slow){
             fast = getSum(getSum(fast));
             slow = getSum(slow);
         }
-        
-        
+                
         return fast==1 ; //이 연산자의 결과 boolean 값이 return 됨 
     }
     
@@ -56,5 +56,34 @@ class Solution {
         return sumSquaresOfDigits;
     }
 }
+*/
+//first Solution -using HashSet . (계산결과값이 1 이 아니고, HashSet 에 이미 값이 있다면, cycle)
+class Solution {
+    public boolean isHappy(int n) {
+        if(n <=0){
+            return false;
+        }
+        
+        Set<Integer> sumResult = new HashSet<>();
 
-//first Solution
+        while(n != 1){
+            n = getSum(n);
+            if(sumResult.contains(n)){
+                return false;
+            }else{
+                sumResult.add(n);
+            }
+        }
+        return true;
+    }
+    
+    int getSum(int n){
+        int sumSquareDigits = 0;
+        while(n > 0){
+            int digit = n % 10;
+            sumSquareDigits += digit * digit;
+            n = n / 10;
+        }
+        return sumSquareDigits;
+    }
+}
