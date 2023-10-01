16f355d39979db2e058bcfca05c65ce76630480c
Hye Kyung Ko <73615176+csis2020@users.noreply.github.com>
2023-10-01 16:25:36 -0700

    Time: 0 ms (100.00%), Space: 39.2 MB (59.04%) - LeetHub

--- 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.java
+++ 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.java
@@ -1,3 +1,38 @@
+
+//2023-10-01
+//Discuss 에서 풀이한 간단한 방법 -> 코드는 간단해 졌고, 이해하는데는 시간을 좀 써야 했음
+//      여기서는 숫자를 하나하나 list 에 옮길 필요없이 숫자 그대로 풀이가 가능함. /
+//Time Complexity: O(N) , N is number of digits of 'num'. Max is 9
+class Solution {
+    public int divisorSubstrings(int num, int k) {
+        if(num == 0 || k == 0){
+            return 0;
+        }
+        
+        int kBeauty = 0;
+        int divisor = 0;
+        int power = 1; //power of 10
+        
+        for(int value = num; value > 0; value = value/10 ){
+            divisor += value % 10 * power;
+            k--;
+            if(k > 0){
+                power *= 10;
+            }else{
+                if( divisor != 0 &&  num%divisor == 0 ){
+                    kBeauty++;
+                }
+                divisor = divisor / 10;
+            }
+        }
+        return kBeauty;
+    }
+}
+
+//내가 풀이한 방법 - num 의 숫자들을 하나하나 list 에 담음 
+//Time Complexity:O(N) , N is number of digits of 'num', Max is 9
+//Space Complexity: O(N)
+/*
 class Solution {
     public int divisorSubstrings(int num, int k) {
         if(num == 0 || k == 0){
@@ -34,3 +69,4 @@ class Solution {
         return kBeauty;
     }
 }
+*/
