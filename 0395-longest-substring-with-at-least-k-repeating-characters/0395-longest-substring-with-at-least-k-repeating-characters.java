4f79ba433373465b14b228e6b5358a4e59c6a619
Hye Kyung Ko <73615176+csis2020@users.noreply.github.com>
2023-10-01 20:28:48 -0700

    Time: 1 ms (78.15%), Space: 41.6 MB (31.82%) - LeetHub

--- 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.java
+++ 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.java
@@ -5,7 +5,7 @@
 //
 //Time Complexity: O(N) , 정확하게는 O( N * maxUnique)인데 maxUnique 는 최대경우가 26으로 fix 된 값으로 생략가능
 //Space Complexity: O(1) 알파벳 체크하는 사이즈 26으로 fix 된 배열만 잡고 쓰므로.
-
+/*
 class Solution {
     public int longestSubstring(String s, int k) {
         if(s == null || s.length() <= 0) {
@@ -73,8 +73,8 @@ class Solution {
         return charSet.size();
     }
 }
+*/
 
-/*
  class Solution {
     public int longestSubstring(String s, int k) {
       int [] alpha = new int[26];
@@ -108,7 +108,7 @@ class Solution {
       
     }
   }
-  */
+
 //brute force -이 방법도 submit 하면 Accepted 됨. 
 //idea : for 문 2번 돌면서 [i]부터[j]까지 범위에 있는 character 들이 같은alpahbet 이 k개 이상인지 check
 //Time Complexity: O(N^2) , 같은 알파벳이 k개 이상인지 체크하는것은 늘 26개 체크해서 계산에서 제외
