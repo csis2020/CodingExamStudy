40f9989bccb803463ef7c78919821a159be8739e
Hye Kyung Ko <73615176+csis2020@users.noreply.github.com>
2023-10-01 19:19:25 -0700

    Time: 46 ms (64.61%), Space: 55 MB (24.79%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -3,6 +3,35 @@
 //Idea:  HashMap + Sliding Window => 여기서 HashMap<Value, Value 나타난횟수>
 //Time Complexity : O(N)
 //Space Complexity : O(1) , HashSet 사이즈가 2 로 고정.
+
+class Solution {
+    public int totalFruit(int[] fruits) {
+        
+        if(fruits == null || fruits.length <= 0){
+            return 0;
+        }
+        
+        Map<Integer, Integer> basket = new HashMap<>();
+        int start = 0;
+        int longest = 0;
+        for(int end = 0; end < fruits.length; end++){
+            basket.put(fruits[end], basket.getOrDefault(fruits[end], 0) + 1);
+            while(basket.size() > 2){
+                int key = fruits[start];
+                basket.put(key, basket.get(key) - 1);
+                if(basket.get(key) == 0){
+                    basket.remove(key);
+                }
+                start++;
+            }
+            
+            longest = Math.max(longest, end - start + 1);
+        }
+        return longest;
+    }
+}
+
+/*
 class Solution {
     public int totalFruit(int[] fruits) {
         
@@ -31,3 +60,4 @@ class Solution {
         return longest;
     }
 }
+*/
