3cbc6ea271e70c8a7671ee094f71acb461b8aef1
csis2020 <73615176+csis2020@users.noreply.github.com>
2023-10-05 14:33:08 -0700

    merge intervals

---
+++ 2023-10-04_643_Maximum_Average_Subarray_I.java
@@ -0,0 +1,32 @@
+
+//2023-10-04
+//idea : window size = k, keep moving and find max average 
+// [example] Input: nums = [1,12,-5,-6,50,3], k = 4
+// <1, 12, -5, -6> -> <12, -5, -6, 50> -> <-5, -6, 50, 3>
+
+//Time Complexity: O(N)
+//Space Complexit: O(1)
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        
+        if(nums == null || nums.length < k|| k <= 0){
+            return 0;
+        }
+        
+        double curSum = 0;
+
+        for(int i = 0; i < k; i++){
+            curSum += nums[i];
+        }
+        
+        double maxSum = curSum;
+      
+        for(int i = k; i < nums.length; i++){
+            curSum += nums[i];
+            curSum -= nums[i - k];
+            maxSum = Math.max(maxSum, curSum);
+        }
+        return maxSum / k;
+        
+    }
+}
