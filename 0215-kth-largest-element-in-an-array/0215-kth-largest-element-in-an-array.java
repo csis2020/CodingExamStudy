d03d1098453ecc791bf8864422adc65b605dd456
Hye Kyung Ko <73615176+csis2020@users.noreply.github.com>
2023-10-07 08:41:06 -0700

    Time: 22 ms (88.27%), Space: 56.5 MB (29.77%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -11,9 +11,14 @@ class Solution {
             return -10001;
         }
         
-        //Arrays.sort(nums, Collections.reverseOrder());
+        //1차원 배열을 입력으로 받는 경우, Comparator 를 따로 넣을수가 없다. Comparator 의 입력타입이 
+        // object 이어야 함. int[] nums 가 아니라 Integer[] nums 이면 동작됨. 
+        // 되게 하는 방법이 있을까? 나중에 고민해봐야할 듯. 
+        //Arrays.sort(nums, (a,b)->(b-a)); <-- 동작 안함. 에러발생
+        //Arrays.sort(nums, Collections.reverseOrder()); <--- 이동작 안함. 에러발생
         Arrays.sort(nums);
         
         return nums[nums.length -k];
     }
+    
 }
