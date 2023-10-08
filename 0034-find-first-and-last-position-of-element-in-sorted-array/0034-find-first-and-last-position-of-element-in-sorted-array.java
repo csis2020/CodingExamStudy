60c6793b13ae3ef0ddad743b05f6f7c43c4c9934
Hye Kyung Ko <73615176+csis2020@users.noreply.github.com>
2023-10-08 16:48:55 -0700

    Time: 0 ms (100.00%), Space: 44.4 MB (38.52%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -2,6 +2,71 @@
 //Input: nums = [5,7,7,8,8,10], target = 8 -> Output: [3,4]
 //Input: nums = [], target = 0 -> Output: [-1,-1]
 //Input: nums = [2,2], target = 3 -> Output: [-1,-1]
+
+//2023-10-08 
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        if(nums == null || nums.length <= 0){
+            return new int[]{-1, -1};
+        }
+        
+        //find left boundary
+        int leftBoundary = -1;
+        int rightBoundary = -1;
+        int start = 0; 
+        int end = nums.length -1;
+        int mid = 0;
+        //nums = [5,7,7,8,8,10], target = 8
+        //(start,end):(0,5),(3,5),(3,3)
+        //mid: 2, 4, 3 -> leftBoundary = 3
+        while(start <= end){
+            mid = (start + end) /2;
+            if(nums[mid] == target){
+                if(mid > 0 && nums[mid -1] == target){
+                    end = mid -1;
+                }else{
+                   leftBoundary = mid;
+                    break;
+                }
+            }else if(nums[mid] > target){
+                end = mid -1;
+            }else{
+                start = mid + 1;
+            }            
+        }
+        
+        //find right boundary
+
+        start = (leftBoundary > 0) ? leftBoundary : 0;
+        end = nums.length -1;
+        //nums = [5,7,7,8,8,10], target = 8
+        //(start,end):(3,5)
+        //mid: 4 -> rightBounday : 4
+        while(start <= end){
+            mid = (start + end) /2;
+            if(nums[mid] == target){
+                if(mid < (nums.length -1) && nums[mid+1] == target){
+                    start = mid + 1;
+                }else{
+                    rightBoundary = mid;
+                    break;
+                }
+            }else if(nums[mid] > target){
+                end = mid -1;
+            }else{
+                start = mid + 1;
+            }
+        }
+        
+        return new int[]{leftBoundary, rightBoundary};
+    }
+}
+
+//2023-10-08 
+//idea: First, find mid position using binary search. The, find the boundary of start and end
+//Time Complexity: O(N) <-- 시작은 binary search 로 O(logN) 이였지만 양쪽 boundary 를 찾는 과정에서 최악의 경우 O(N)임 예를들어  nums = [8,8,8,8,8,8,8,8], target = 8
+//Space Complexity: O(1)
+/*
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         
@@ -50,3 +115,5 @@ class Solution {
         return -1;
     }
 }
+
+*/
