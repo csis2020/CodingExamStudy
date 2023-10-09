78b898fdd0041935df115dec921d7f9c012f04c9
Hye Kyung Ko <73615176+csis2020@users.noreply.github.com>
2023-10-08 19:43:04 -0700

    Time: 44 ms (94.73%), Space: 100.9 MB (11.49%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -24,39 +24,6 @@
 //idea: end 값을 기준으로 sorting 한뒤 겹치면 앞에 있는것 말고 뒤에 있는 것을 지우는게 확률상 최소로 지우며 non-overlapping 을 만들수 있다 =>  몰랐었음... 이걸 기억해야함. 
 //Time Complexity: O(NlogN) <-- sort time 
 //Space Complexity: O(N)<-- the space for sorting
-
-class Solution{
-    public int eraseOverlapIntervals(int[][] intervals){
-        if(intervals == null || intervals.length <= 0 || intervals[0].length <= 0){
-            return 0;
-        }
-        
-        Arrays.sort(intervals, (a,b)->(a[0]-b[0]) ); //sort by 'start' element
-        
-        int remove = 0;
-        //int prevEnd = Integer.MIN_VALUE;
-        //만약-2^31 <= xstart < xend <= 2^31 - 1 라고 boundary 가 정해진경우
-        // prevEnd = Integer.MIN_VALUE 로 놓고 for 문을 0 부터 시작하면 
-        //input: [[-2147483648,2147483647]] 인 경우 문제 생김. 
-        int prevEnd = intervals[0][1];
-        //for(int i = 0; i < intervals.length; i++){
-        for(int i = 1; i < intervals.length; i++){
-            int curStart = intervals[i][0];
-            int curEnd = intervals[i][1];
-            if(prevEnd > curStart){
-                remove++;
-                
-                if(prevEnd > curEnd){
-                    prevEnd = intervals[i][1];
-                } 
-            }else {
-                prevEnd = intervals[i][1];
-            }            
-            
-        }
-        return remove;
-    }
-}
 /*
 class Solution{
     public int eraseOverlapIntervals(int[][] intervals){
@@ -85,6 +52,71 @@ class Solution{
 }
 */
 
+//intervals = [[1,2],[2,3],[3,4],[1,3]] => [[1,2],[2,3],[1,3],[3,4]]
+class Solution{
+    public int eraseOverlapIntervals(int[][] intervals){
+        if(intervals == null || intervals.length <= 0){
+            return 0;
+        }
+        
+        Arrays.sort(intervals, (a,b)->(a[1]- b[1])); // ordered by the end position - ascending order
+        int remove = 0;
+        int pos = 0;
+        for(int i = 1; i < intervals.length; i++){
+            int[] current = intervals[i];
+            int[] previous = intervals[pos];
+            if(current[0] < previous[1]){
+                remove++;
+            }else{
+                pos = i;
+            }            
+        }
+        
+        return remove;
+    }
+}
+
+// [아래는 start 를 기준으로 sorting 한 경우 풀이 방법]
+// end 로 sorting 한 경우는 prevEnd 가 당연히 더 작으나, 
+// start로 sorting 한 경우는 어느쪽 end 가 더 작은지 알수가 없어서 prevEnd 가 curEnd 보다 크면 
+// 더 작은 end 를 가르키도록 prevEnd 를 옮겨주어야 한다. 
+/*
+class Solution{
+    public int eraseOverlapIntervals(int[][] intervals){
+        if(intervals == null || intervals.length <= 0 || intervals[0].length <= 0){
+            return 0;
+        }
+        
+        Arrays.sort(intervals, (a,b)->(a[0]-b[0]) ); //sort by 'start' element
+        
+        int remove = 0;
+        //int prevEnd = Integer.MIN_VALUE;
+        //만약-2^31 <= xstart < xend <= 2^31 - 1 라고 boundary 가 정해진경우
+        // prevEnd = Integer.MIN_VALUE 로 놓고 for 문을 0 부터 시작하면 
+        //input: [[-2147483648,2147483647]] 인 경우 문제 생김. 
+        int prevEnd = intervals[0][1];
+        //for(int i = 0; i < intervals.length; i++){
+        for(int i = 1; i < intervals.length; i++){
+            int curStart = intervals[i][0];
+            int curEnd = intervals[i][1];
+            if(prevEnd > curStart){
+                remove++;
+                // end 가 더 큰쪽을 remove 해야 한다.  (실제로 remove 하는게 아니라 check 대상에서 제외하는 효과 )
+                // end 로 sorting 한 경우는 prevEnd 가 당연히 더 작으나, 
+                // start로 sorting 한 경우는 어느쪽 end 가 더 작은지 알수가 없어서 prevEnd 가 curEnd 보다 크면 더 작은 end 를 가르키도록 prevEnd 를 옮겨주어야 한다. 
+                if(prevEnd > curEnd){
+                    prevEnd = intervals[i][1];
+                } 
+            }else {
+                prevEnd = intervals[i][1];
+            }            
+            
+        }
+        return remove;
+    }
+}
+*/
+    
 //2022-12-05
 //Time Complexity: O(nlon) <-- sort time 
 //Space Complexity: O(logn) or O(n) <-- the space for sorting
