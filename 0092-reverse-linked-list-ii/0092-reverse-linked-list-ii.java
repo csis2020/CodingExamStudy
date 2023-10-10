5460bf56c970509c4dc21b5a5b44078ac32f41a4
Hye Kyung Ko <73615176+csis2020@users.noreply.github.com>
2023-10-10 12:47:18 -0700

    Time: 0 ms (100.00%), Space: 40 MB (67.19%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -56,6 +56,7 @@ class Solution {
 }
 */
 //위의 idea 로 leetcode solution이 만든 조금더 깔끔하게 정리된 코드 
+/*
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
         
@@ -86,5 +87,102 @@ class Solution {
         before.next = prev; //prev : head of reversed list
         
         return dummy.next;
+    }
+}
+*/
+
+//recursive solution -leetcode
+/*
+class Solution {
+
+    // Object level variables since we need the changes
+    // to persist across recursive calls and Java is pass by value.
+    private boolean stop;
+    private ListNode left;
+
+    public void recurseAndReverse(ListNode right, int m, int n) {
+
+        // base case. Don't proceed any further
+        if (n == 1) {
+            return;
+        }
+
+        // Keep moving the right pointer one step forward until (n == 1)
+        right = right.next;
+
+        // Keep moving left pointer to the right until we reach the proper node
+        // from where the reversal is to start.
+        if (m > 1) {
+            this.left = this.left.next;
+        }
+
+        // Recurse with m and n reduced.
+        this.recurseAndReverse(right, m - 1, n - 1);
+
+        // In case both the pointers cross each other or become equal, we
+        // stop i.e. don't swap data any further. We are done reversing at this
+        // point.
+        if (this.left == right || right.next == this.left) {
+            this.stop = true;            
+        }
+
+        // Until the boolean stop is false, swap data between the two pointers
+        if (!this.stop) {
+            int t = this.left.val;
+            this.left.val = right.val;
+            right.val = t;
+
+            // Move left one step to the right.
+            // The right pointer moves one step back via backtracking.
+            this.left = this.left.next;
+        }
+    }
+
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        this.left = head;
+        this.stop = false;
+        this.recurseAndReverse(head, m, n);
+        return head;
+    }
+}
+*/
+
+//recursive solution - my idea 
+//위 처럼 전역변수를 둘꺼면, '206. Reverse Linked List' 의 방법을 이용해도 될거 같아서 그렇게 해본 내용
+class Solution {
+    ListNode after;
+    
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        
+        if(head == null || head.next == null){
+            return head;
+        }
+        
+        ListNode dummy = new ListNode(0, head);
+        ListNode before = dummy;
+        int count = 1;
+        while(count < left){
+            before = before.next;
+            count++;
+        }
+        
+        ListNode reversedHead = recursiveReverse(before.next, right -left);
+        before.next = reversedHead;
+        
+        return dummy.next;
+        
+    }
+    
+    ListNode recursiveReverse(ListNode head, int count){
+        if(head.next == null || count == 0){
+            after = head.next;
+            return head;            
+        }
+        
+        ListNode nextNode = head.next;
+        ListNode reversedHead = recursiveReverse(nextNode, count -1);
+        nextNode.next = head;
+        head.next = after;
+        return reversedHead;
     }
 }
