5658d7abd360189e879ac31c901ad4bce8e54139
Hye Kyung Ko <73615176+csis2020@users.noreply.github.com>
2023-10-20 13:19:19 -0700

    Time: 1 ms (69.43%), Space: 41.1 MB (53.75%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -19,8 +19,8 @@
 //Idea: bfs search using queue
 
 //Time Complexity: O(N)
-//Space Complexity: O(N)
-
+//Space Complexity: O(N), 좀더 자세히는 O(D), D is a tree diameter. Worst case is D = N/2 => O(N)
+/*
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> result = new ArrayList<>();
@@ -52,5 +52,40 @@ class Solution {
         }
         
         return result;
+    }
+}
+*/
+
+//idea2: DFS  (leetcode solution 에 BFS 와 DFS 둘다 있음. 나는 BFS 로 했고, DFS 는 솔루션을 보고 만듬. )
+//Time Complexity: O(N)
+//Space Complexity: O(N), 좀더 자세히는 O(H), H is tree height. 그러나 역시 Worst case 의 경우 H = N 임 
+
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if(root == null){
+            return result;
+        }
+        
+        dfs(root, 0, result);
+        return result;
+    }
+    
+    //[1,2,3,null,5,null,4]
+    //root: 1,
+    //level:0,  
+    //result:[]->[1]->[1,3]->[1,3,4]
+    void dfs(TreeNode root, int level, List<Integer> result){
+        
+        if(root == null){
+            return;
+        }
+        
+        if(level == result.size()){
+            result.add(root.val);
+        }
+        
+        dfs(root.right, level+1, result);
+        dfs(root.left, level+1, result);
     }
 }
