3fc052bf43d2e38f58d1925e7b6e05c377b3b91a
Hye Kyung Ko <73615176+csis2020@users.noreply.github.com>
2023-10-20 11:48:05 -0700

    Time: 0 ms (100.00%), Space: 43.1 MB (62.93%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
@@ -21,6 +21,45 @@ class Node {
 };
 */
 
+//Queue 에 null 을 안쓰고 하는 경우 
+//Time Complexity: O(N)
+//Space Complexity: O(N)
+/*
+class Solution {
+    public Node connect(Node root) {
+                if(root == null){
+            return null;
+        }
+         
+        Queue<Node> bfsQueue = new LinkedList<>();
+        bfsQueue.add(root);        
+       
+        while(!bfsQueue.isEmpty()){
+           
+            int size = bfsQueue.size();
+            Node prev = null;
+            for(int i = 0; i < size; i++){
+                Node leave = bfsQueue.remove();
+                if(prev != null){
+                    prev.next = leave;                    
+                }
+                
+                if(leave.left != null){
+                    bfsQueue.add(leave.left);
+                }
+                if(leave.right != null){
+                    bfsQueue.add(leave.right);
+                }   
+                prev = leave;
+            }
+        }
+            
+        return root;
+    }
+}
+*/
+
+//Queue 에 null 을 넣어서 하는 경우 
 //Time Complexity: O(N)
 //Space Complexity: O(N)
 /*
@@ -77,36 +116,54 @@ class Solution {
 }
 */
 
-
+//idea2:  Using previously established next pointers (leetcode solution)
+// (궁금증: full binary tree 라서 가능한 솔루션일까??? )
+/*
+ leftmost = root
+ while (leftmost.left != null)
+ {
+     head = leftmost
+     while (head != null)
+     {
+         1) Establish Connection 1
+         2) Establish Connection 2 using next pointers
+         head = head.next
+     }
+     leftmost = leftmost.left
+ }
+*/
+// Time Complexity: O(N)
+// Space Complexity: O(1)
 class Solution {
     public Node connect(Node root) {
-                if(root == null){
+        if(root == null){
             return null;
         }
-         
-        Queue<Node> bfsQueue = new LinkedList<>();
-        bfsQueue.add(root);        
-       
-        while(!bfsQueue.isEmpty()){
-           
-            int size = bfsQueue.size();
-            Node prev = null;
-            for(int i = 0; i < size; i++){
-                Node leave = bfsQueue.remove();
-                if(prev != null){
-                    prev.next = leave;                    
-                }
-                
-                if(leave.left != null){
-                    bfsQueue.add(leave.left);
-                }
-                if(leave.right != null){
-                    bfsQueue.add(leave.right);
-                }   
-                prev = leave;
-            }
-        }
+        
+        Node leftMost = root;
+        
+        //[1,2,3,4,5,6,7]
+        //leftMost:1, 2, 4
+        //leftMost.left:2, null
+        //head:1, 2, 3
+        //next: 2->3, 4->5->6->7
+        while(leftMost.left != null){
             
+            Node head = leftMost;
+            while(head != null){
+                
+                if(head.left != null){
+                    head.left.next = head.right;
+                }
+
+                if(head.next != null){
+                    head.right.next = head.next.left; 
+                }
+                head = head.next;
+            }
+            leftMost = leftMost.left;
+        }
+        
         return root;
     }
 }
