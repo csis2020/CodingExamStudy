55529517780e3a49bdd23b98fa0c05ed27a0e53c
Hye Kyung Ko <73615176+csis2020@users.noreply.github.com>
2023-10-21 21:35:37 -0700

    Time: 1 ms (96.14%), Space: 43.7 MB (25.39%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -16,6 +16,11 @@
 
 //2023-10-21
 //idea : preorder 의 첫 시작이 root,  inorder 에서 root 를 찾으면, root 전까지 left tree, root 앞은 right tree => recursive 함수 이용 
+//Time Complexity: O(N^2) <-- root 의 search 타임이 있음.  N^2 이 됨.
+//      그래서 leetcode solution에서는 inorder배열을 hashSet 에 넣어서 search time 을 없에고 O(N)으로 만들었다. 대신 Space complexity 는 O(N), 그러나 어차피 원래도 Space complexity 의 worst case 는 O(N)이긴 했으니 손해가 아니네.
+//Space Complexity: O(H) <- recursive 함수의 stack size 는 tree 높이 
+
+/*
  class Solution{
      public TreeNode buildTree(int[] preorder, int[] inorder){
          if(preorder == null || preorder.length == 0 || inorder == null || inorder.length == 0){
@@ -51,19 +56,92 @@
      //sizeRight:3,1
      //root:1, 3
      //TREE: 3-(9,20-(15,7))
+         
          int sizeOfLeftTree = rootPosition - inStart;
          int sizeOfRightTree = inEnd - rootPosition;
          node.left = makeTree(preorder, inorder, preStart+1, preStart + sizeOfLeftTree, inStart, rootPosition -1 );
          node.right = makeTree(preorder, inorder, preStart + sizeOfLeftTree + 1, preEnd, rootPosition +1, inEnd );
-             
+         
         return node;
      }
  }
+*/
+
+//idea: 기본 개념은 위에 내가 푼것과 동일하고, time complexity 를 줄이기 위해 inorder 를 hashMap 에 넣음. 이를 통해서 시간도 줄임.
+//Time Complexity: O(N) 
+//Space Complexity: O(N) <- HashMap size
+ class Solution{
+     public TreeNode buildTree(int[] preorder, int[] inorder){
+         if(preorder == null || preorder.length == 0 || inorder == null || inorder.length == 0){
+             return null;
+         }
+         
+         HashMap<Integer, Integer> inorderMap = new HashMap<>();
+         for(int i = 0; i < inorder.length; i++){
+             inorderMap.put(inorder[i], i);
+         }
+         
+         
+         return makeSubTree(preorder, inorderMap, 0, preorder.length -1, 0, inorder.length -1);
+     }
+    
+     TreeNode makeSubTree(int[] preorder, HashMap<Integer, Integer> inorderMap, int preStart, int preEnd, int inStart, int inEnd ){
+         if(preStart > preEnd || inStart > inEnd ){
+             return null;
+         }
+         
+        int rootValue = preorder[preStart];
+         TreeNode node = new TreeNode(rootValue);
+        
+         if(preStart == preEnd){
+             return node;
+         }
+         
+         int mid = inorderMap.get(rootValue);
+         
+         int leftSize = mid - inStart;
+         int rightSize = inEnd - mid;
+         node.left = makeSubTree(preorder, inorderMap, preStart+1, preStart + leftSize, inStart, mid -1);
+         node.right = makeSubTree(preorder, inorderMap, preStart + leftSize +1, preEnd,  mid+1, inEnd);
+        
+        return node;
+     }
+    
+ }
 
 
+//아래는 leetcode solution 인데, preorderIndex 를 따로 두고 1씩 count 하는게 잘 이해가 되지 않았다. 그냥 내가 하던 방식대로 하는게 나을듯. 
+/*
+class Solution {
+    int preorderIndex;
+    Map<Integer, Integer> inorderIndexMap;
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        preorderIndex = 0;
+        // build a hashmap to store value -> its index relations
+        inorderIndexMap = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            inorderIndexMap.put(inorder[i], i);
+        }
 
+        return arrayToTree(preorder, 0, preorder.length - 1);
+    }
 
+    private TreeNode arrayToTree(int[] preorder, int left, int right) {
+        // if there are no elements to construct the tree
+        if (left > right) return null;
 
+        // select the preorder_index element as the root and increment it
+        int rootValue = preorder[preorderIndex++];
+        TreeNode root = new TreeNode(rootValue);
+
+        // build left and right subtree
+        // excluding inorderIndexMap[rootValue] element because it's the root
+        root.left = arrayToTree(preorder, left, inorderIndexMap.get(rootValue) - 1);
+        root.right = arrayToTree(preorder, inorderIndexMap.get(rootValue) + 1, right);
+        return root;
+    }
+}
+*/
 
 
 
