9d35c595a458c8d240219ae15de4661e850be695
Hye Kyung Ko <73615176+csis2020@users.noreply.github.com>
2023-10-22 00:15:57 -0700

    Time: 11 ms (14.73%), Space: 44.9 MB (98.99%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
@@ -18,6 +18,7 @@
 //idea: BFS using Queue
 //Time Complexity: O(N)
 //Space Complexity:O(D)  , D is tree's diameter. the worst case is O((N+1)/2) = O(N)
+/*
 class Solution {
     public int maxLevelSum(TreeNode root) {
         if(root == null){
@@ -50,5 +51,49 @@ class Solution {
         }
         
         return maxSumLevel;
+    }
+}
+*/
+
+//idea2 - DFS - recursive with List<> 
+class Solution {
+    
+    public int maxLevelSum(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        
+        List<List<Integer>> sumList = new ArrayList<>();
+        makeSumList(root, sumList, 0);
+        
+        int maxSum = -100001;
+        int maxSumLevel = 0;
+        for(int i = 0; i < sumList.size(); i++){
+            List<Integer> list = sumList.get(i);
+            int sum = 0;
+            for(int j = 0; j < list.size(); j++){
+                sum += list.get(j);
+            }
+            if(sum > maxSum){
+                maxSum = sum;
+                maxSumLevel = i + 1;
+            }
+        }
+        
+        return maxSumLevel;
+    }
+    
+    void makeSumList(TreeNode root, List<List<Integer>> sumList, int depth){
+        if(root == null){
+            return;
+        }
+        
+        if(sumList.size() == depth){
+            List<Integer> list = new ArrayList<>();
+            sumList.add(list);
+        }
+        sumList.get(depth).add(root.val);
+        makeSumList(root.left, sumList, depth+1);
+        makeSumList(root.right, sumList, depth+1);        
     }
 }
