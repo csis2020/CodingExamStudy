62f7091f889e4ed1f79d43a46ceff04970a7cce3
Hye Kyung Ko <73615176+csis2020@users.noreply.github.com>
2023-10-22 19:32:47 -0700

    Time: 1 ms (69.75%), Space: 41.1 MB (53.88%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -14,12 +14,51 @@
  * }
  */
 
+//2023-10-22
+//Time Complexity: O(N)
+//Space Complexity: O(N), 좀더 자세히는 O(D), D is a tree diameter. Worst case is D = (N+1)/2 => O(N)
+//Idea1: BFS - using queue
+//[1] -> [2,3] -> [5, 4]
+//[1,3,4]
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if(root == null){
+            return result;
+        }
+        
+        Queue<TreeNode> bfs = new LinkedList<>();
+        bfs.add(root);
+        //[1,2,3,null,5,null,4]
+        //queue:[1]->[2,3]->[5,4]->[]
+        //size:1,2,2
+        //node:1,2,3, 5,4
+        //list: <1,3,4>
+        while(!bfs.isEmpty()){
+            int size = bfs.size();
+            TreeNode node = null;
+            for(int i = 0; i < size; i++){
+                node = bfs.remove();
+                if(node.left != null){
+                    bfs.add(node.left);
+                }
+                if(node.right != null){
+                    bfs.add(node.right);
+                }
+            }
+            result.add(node.val);
+        }
+        return result;
+    }
+}
+
+//2023-10-20
 //limitation : Is it full binary tree?? No.
 
 //Idea: bfs search using queue
 
 //Time Complexity: O(N)
-//Space Complexity: O(N), 좀더 자세히는 O(D), D is a tree diameter. Worst case is D = N/2 => O(N)
+//Space Complexity: O(N), 좀더 자세히는 O(D), D is a tree diameter. Worst case is D = (N+1)/2 => O(N)
 /*
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
@@ -59,7 +98,7 @@ class Solution {
 //idea2: DFS  (leetcode solution 에 BFS 와 DFS 둘다 있음. 나는 BFS 로 했고, DFS 는 솔루션을 보고 만듬. )
 //Time Complexity: O(N)
 //Space Complexity: O(N), 좀더 자세히는 O(H), H is tree height. 그러나 역시 Worst case 의 경우 H = N 임 
-
+/*
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> result = new ArrayList<>();
@@ -89,3 +128,4 @@ class Solution {
         dfs(root.left, level+1, result);
     }
 }
+*/
