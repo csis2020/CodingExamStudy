f6e69f84ce2d05f9752b52e1b16cb7bf139c5d0d
Hye Kyung Ko <73615176+csis2020@users.noreply.github.com>
2023-10-25 20:13:01 -0700

    Time: 1 ms (98.60%), Space: 43.9 MB (76.68%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.java
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.java
@@ -18,6 +18,7 @@
 //Idea: using BFS with Queue
 //Time Complexity: O(N)
 //Space Complexity: O(N) <-- the worst case is the full binary tree's bottom width = (N+1)/2 => O(N)
+/*
 class Solution {
     public List<Integer> largestValues(TreeNode root) {
         List<Integer> list = new ArrayList<>();
@@ -46,6 +47,40 @@ class Solution {
         }
         
         return list;
+        
+    }
+}
+*/
+
+//Idea: using DFS (preorder traversal) using recursive call
+//Time Complexity: O(N)
+//Space Complexity: O(N) <-- the worst case is the skewed tree ( the tree only has left nodes or only has right nodes.  ->O(N) )
+class Solution {
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        
+        if(root == null){
+            return list;
+        }
+        
+        dfs(root, list, 0);
+        return list;
+    }
+    
+    void dfs(TreeNode root, List<Integer> list, int row){
+        if(root == null){
+            return;
+        }
+        
+        if(list.size() > row){
+            int value = list.get(row);
+            list.set(row, Math.max(value, root.val));            
+        }else if(list.size() == row){
+            list.add(root.val);
+        }
+        
+        dfs(root.left, list, row+1);
+        dfs(root.right, list, row+1);
         
     }
 }
