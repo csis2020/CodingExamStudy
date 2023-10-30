5613e3e50e4623b38f102a10df7f2806a106739b
Hye Kyung Ko <73615176+csis2020@users.noreply.github.com>
2023-10-29 22:11:35 -0700

    Time: 0 ms (100.00%), Space: 43.6 MB (77.49%) - LeetHub

---
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
@@ -0,0 +1,79 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+//2023-10-29
+//idea : BFS using queue - 단, right node , left node 순으로 queue 에 넣기
+//Time Complexity: O(N)
+//Space Complexity: O(N) - tree's max width - the worst case: (N+1)/2
+/*
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        if(root == null){
+            return Integer.MIN_VALUE;
+        }
+        
+        Queue<TreeNode> bfs = new LinkedList<>();
+        bfs.add(root);
+        int btmLeftValue = Integer.MIN_VALUE;
+        while(!bfs.isEmpty()){
+            int size = bfs.size();
+            TreeNode node = null;
+            for(int i = 0; i < size; i++){
+                node = bfs.remove();
+                if(node.right != null){
+                    bfs.add(node.right);
+                }
+                if(node.left != null){
+                    bfs.add(node.left);
+                }
+            }
+            btmLeftValue = node.val;
+        }
+        
+        return btmLeftValue;
+    }
+}
+*/
+
+//idea2: DFS using recursive call - postorder traversal (left-right-root 순으로 )
+//Time Complexity: O(N)
+//Space Complexity: O(N) : tree's height - the average case is O(logN), the worst case is O(N)
+class Solution {
+    int maxLevel = 0;
+    int bottomLeftValue = Integer.MIN_VALUE;
+    public int findBottomLeftValue(TreeNode root) {
+        if(root == null){
+            return Integer.MIN_VALUE;
+        }
+        
+        dfs(root, 0);
+        return bottomLeftValue;
+    }
+    
+    void dfs(TreeNode root, int level){
+        if(root == null){
+            return;
+        }
+        
+        if(maxLevel <= level){
+            maxLevel = level;
+            bottomLeftValue = root.val;
+        }
+        
+        dfs(root.right, level+1);
+        dfs(root.left, level+1);
+    }
+}
