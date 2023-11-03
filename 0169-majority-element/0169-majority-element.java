436f905cb01c5571d6e13c3a1c0e657d98a05228
Hye Kyung Ko <73615176+csis2020@users.noreply.github.com>
2023-11-03 13:38:11 -0700

    Time: 1 ms (99.90%), Space: 48.6 MB (23.32%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -32,6 +32,7 @@ class Solution {
 //Idea2 : Sorting 
 //Time Complexity: O(NlogN)
 //Space Complexity: O(1)
+/*
 class Solution {
     public int majorityElement(int[] nums) {
         if(nums == null || nums.length <= 0){
@@ -40,5 +41,32 @@ class Solution {
         
         Arrays.sort(nums); // ascending order
         return nums[nums.length/2];
+    }
+}
+*/
+
+//Idea3: Moore voting algorithm 
+// 이 방법은 Majority element 가 n/2 보다 많은 경우 성립함. ( the number of majority > n/2)  , the  number of majority >= n/2 이면 성립 안됨. 
+//Time Complexity: O(N)
+//Space Complexity: O(1)
+class Solution {
+    public int majorityElement(int[] nums) {
+        if(nums == null || nums.length <= 0){
+            return Integer.MIN_VALUE;
+        }
+        
+        int majorityNum = nums[0];
+        int count = 1;
+        for(int i = 1; i < nums.length; i++){
+            if(count == 0){
+                majorityNum = nums[i];
+                count++;
+            }else if(majorityNum == nums[i]){
+                count++;
+            }else {
+                count--;
+            }
+        }
+        return majorityNum;
     }
 }
