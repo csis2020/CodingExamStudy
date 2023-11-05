3c243468ec7b9a1491bcb03f6181ca852b413979
Hye Kyung Ko <73615176+csis2020@users.noreply.github.com>
2023-11-05 00:05:39 -0700

    Time: 3 ms (98.99%), Space: 45.7 MB (44.09%) - LeetHub

---
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
@@ -0,0 +1,116 @@
+
+
+
+//2023-11-03
+//idea:  560. Subarray Sum Equals K , 523. Continuous Subarray Sum 과 유사 ???
+//   
+/*
+class Solution{
+    public int subarraysDivByK(int[] nums, int k){
+    }
+}
+*/
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+        int n = nums.length;
+        int prefixMod = 0, result = 0;
+
+        // There are k mod groups 0...k-1.
+        int[] modGroups = new int[k];
+        modGroups[0] = 1;
+
+        for (int num: nums) {
+            // Take modulo twice to avoid negative remainders.
+            prefixMod = (prefixMod + num % k + k) % k;
+            // Add the count of subarrays that have the same remainder as the current
+            // one to cancel out the remainders.
+            result += modGroups[prefixMod];
+            modGroups[prefixMod]++;
+        }
+
+        return result;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//누적합을 이용하여 푸는 문제
+//2-23-01-23--[2]
+//아래 [Java] BOJ 10986 나머지 합 (누적합)  해결법 참조 (백준)
+//https://velog.io/@jodawooooon/Java-BOJ-10986-%EB%82%98%EB%A8%B8%EC%A7%80-%ED%95%A9  
+//input: nums=[4,5,0,-2,-3,1] , k = 5 -> output: 7
+/*
+class Solution{
+    public int subarraysDivByK(int[] nums, int k){
+        if(nums == null || nums.length == 0 || k <= 0){
+            return 0; 
+        }
+        
+        int length = nums.length;
+        int totalCount = 0;
+        //int[] prefixMod = new int[length];
+        //int sum = 0;
+        int prefixMod = 0;
+        int mod[] = new int[k];
+
+        //nums=[4,  5,  0,  -2,  -3,  1] , k = 5
+        //mod   4,  4   4    2    4   0 
+        // mod[0] = 1, mod[1] = 0, mod[2] = 1, mod[3] = 1, mod[4] = 3
+        for(int i = 0; i < length; i++){
+            //sum += nums[i];
+            prefixMod = (prefixMod + nums[i]%k +k) % k; // to make the negative modular number to positive number
+            if(prefixMod == 0){
+                totalCount++;  
+            }
+           mod[prefixMod]++;
+        }
+        //totalCount = mod[0] + 0 + 0 + 0 + 0 + 4*3/2 =  7
+        for(int i = 0; i < k; i++){
+            totalCount += (mod[i] * (mod[i]-1))/2;
+        }
+
+        return totalCount;
+
+    }
+}
+*/
+//2023-01-23---[1]
+//Brute Force 방법 - Time Limit Exceeded
+/*
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+        
+        if(nums == null || nums.length == 0 || k <= 0 ){
+            return 0;
+        }
+
+        int totalCount = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            int sum = 0;
+            for(int j = i; j < nums.length; j++){
+                sum += nums[j];
+                if(sum % k == 0){
+                    totalCount++;
+                }
+            }
+        }
+
+        return totalCount;
+    }
+}
+*/
