ae876a91cc9dbbdd959e1ee28f39bbc6d86b90e9
Hye Kyung Ko <73615176+csis2020@users.noreply.github.com>
2023-11-13 13:15:32 -0800

    Time: 10 ms (95.81%), Space: 91.9 MB (80.65%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,8 +1,9 @@
 
 //2023-11-13
 //Backtracking
-//Time Complexity: O() <-- 조합(Combination)의 개수 일까??
+//Time Complexity: O(n!/(k!(n-k)!)) <-- 조합(Combination)의 개수 일까??
 //Space Complexity: O(K) , recursive call stack's max : K 
+/*
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> result = new ArrayList<>();
@@ -34,5 +35,38 @@ class Solution {
             //    checked[i] = false;
             //}
         }
+    }
+}
+*/
+
+class Solution {
+    private int n;
+    private int k;
+    
+    public List<List<Integer>> combine(int n, int k) {
+        this.n = n;
+        this.k = k;
+        List<List<Integer>> ans = new ArrayList<>();
+        backtrack(new ArrayList<>(), 1, ans);
+        return ans;
+    }
+    
+    public void backtrack(List<Integer> curr, int firstNum, List<List<Integer>> ans) {
+        if (curr.size() == k) {
+            ans.add(new ArrayList<>(curr));
+            return;
+        }
+        
+        int need = k - curr.size();
+        int remain = n - firstNum + 1;
+        int available = remain - need;
+        
+        for (int num = firstNum; num <= firstNum + available; num++) {
+            curr.add(num);
+            backtrack(curr, num + 1, ans);
+            curr.remove(curr.size() - 1);
+        }
+
+        return;
     }
 }
