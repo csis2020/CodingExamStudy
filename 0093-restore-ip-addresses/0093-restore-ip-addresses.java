2f74c79d7b1567cab4952bb24053be7d620cfcc9
Hye Kyung Ko <73615176+csis2020@users.noreply.github.com>
2023-11-13 16:36:18 -0800

    Time: 2 ms (85.20%), Space: 42.1 MB (31.54%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.java
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -1,7 +1,8 @@
 
 //2023-11-13
 //Backtracking
-//Time Complexity: O(2^N * N) <--- N 은 입력 string의 길이, 매 char 마다 자를지말지 2가지 경우의수 * 매 경우의수 끝에 size N 의 string copy. 
+//아래는 내가 생각한 Time Complexity 와 Space Complexity 였는데, leetcode 설명을 보면 다르다. 확인해볼것!
+//Time Complexity: O(2^N * N) ??? <--- N 은 입력 string의 길이, 매 char 마다 자를지말지 2가지 경우의수 * 매 경우의수 끝에 size N 의 string copy. 
 //Space Complexity: O(1) ?? <- recursive call stack 의 max depth 가 4, tempList 의 최대사이즈가 4 이면 상수라서 O(1)일까?
 class Solution {
     public List<String> restoreIpAddresses(String s) {
@@ -35,7 +36,7 @@ class Solution {
             }
             String substring = s.substring(start, end);
             
-            if(substring.length() == 3 && substring.compareTo("255") > 0){
+            if(Integer.parseInt(substring) > 255){
                 break;
             }
             tempList.add(substring);
