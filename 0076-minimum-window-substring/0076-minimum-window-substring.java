a45520745e5a2fe6b7e2b8bc27fc641f91b81894
Hye Kyung Ko <73615176+csis2020@users.noreply.github.com>
2024-02-24 22:06:27 -0800

    Time: 17 ms (45.39%), Space: 45.1 MB (42.64%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,7 +1,71 @@
 
 //2024-02-23
-//Idea: HashMap + Sliding Window
+//2024-02-23
+//Idea: HashMap 2개 (HashMap<Character, set에 주어진 회수>, HashMap<Character, s에 나타난 횟수)+ Sliding Window
+//Time Complexity : O(S의 길이 + t의 길이) <--- 왜 t 의 길이가 더해지지???
+//Space Complexity: O(S의 길이 + t의 길이)
 
+class Solution {
+    public String minWindow(String s, String t) {
+     
+        if(s == null || s.length() <= 0 || t == null || t.length() <= 0 || s.length() < t.length() ){
+            return "";
+        }
+        
+        HashMap<Character, Integer> dictMap = new HashMap<>(); //<character, frequency>
+        HashMap<Character, Integer> currMap = new HashMap<>(); //<Character, current frrequency>
+        
+        //set dictionary
+        for(int i = 0; i < t.length(); i++){
+            char ch = t.charAt(i);
+            dictMap.put(ch, dictMap.getOrDefault(ch, 0) + 1);
+        }
+        
+        int totalCount = 0;
+        int start = 0;
+        String subStr = s;
+        
+        for(int end = 0; end < s.length(); end++){
+            char ch = s.charAt(end);
+            
+            if(dictMap.containsKey(ch)){                
+                currMap.put(ch, currMap.getOrDefault(ch, 0) +1);
+                
+                if(currMap.get(ch) <= dictMap.get(ch)){
+                    totalCount++;
+                }
+                
+                                    
+                if(totalCount == t.length()){
+                    while(start <= end){
+                        char tempCh = s.charAt(start);
+                        if(dictMap.containsKey(tempCh)){
+                            if(dictMap.get(tempCh) >= currMap.get(tempCh)){
+                                break; 
+                            }
+                            currMap.put(tempCh, currMap.get(tempCh) -1);
+                        }
+                        start++;                        
+                    }
+                    
+                    if( (end - start + 1) < subStr.length()){
+                        subStr = s.substring(start, end+1);
+                    }
+                }
+                
+                
+            }
+        }
+        
+    
+        return (totalCount < t.length() ? "" : subStr);
+     }
+}
+
+//Idea: HashMap 1개 (HashMap<Character, int[])+ Sliding Window
+//Time Complexity : O(S의 길이 + t의 길이) <--- 왜 t 의 길이가 더해지지???
+//Space Complexity: O(S의 길이 + t의 길이)
+/*
 class Solution {
     public String minWindow(String s, String t) {
      
@@ -65,3 +129,4 @@ class Solution {
                 
     }
 }
+*/
